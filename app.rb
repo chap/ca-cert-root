2610b51609ff782d0c5c410e71511abe7d4dbcca
Chap Ambrose <chapambrose@gmail.com>
2019-02-14 09:38:47 -0600

    use cacert from curl

--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require "sinatra"
 require 'open-uri'
 
 get '/*' do
-  return open(ENV['TEST_URL'], ssl_ca_cert: '/usr/lib/ssl/certs/ca-certificates.crt').read
+  return open(ENV['TEST_URL'], ssl_ca_cert: 'cacert.pem').read.to_s
 end
